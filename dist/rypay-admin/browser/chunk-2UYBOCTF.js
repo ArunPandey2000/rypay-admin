dfd3b198d0443ffd7077f18bff1c5160b1e8e768
Arun Pandey <apandey@EX2INDIA.LOCAL>
2025-03-26 23:22:21 +0530

    base url updated

---
+++ dist/rypay-admin/browser/chunk-2UYBOCTF.js
@@ -0,0 +1,1 @@
+var e={production:!1,baseUrl:"https://api.riyadhmicrofinance.com",userRoles:["user","admin"]};export{e as a};
--- dist/rypay-admin/browser/chunk-4GPQGFMK.js
+++
@@ -1,1 +0,0 @@
-var e={production:!1,baseUrl:"http://localhost:3000",userRoles:["user","admin"]};export{e as a};
